2e9c57dd61d3c4c6b909c27a191b65bc7ed581d7
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2026-01-08 19:01:38 -0300

    Fix: Add /api prefix to controllers, add RestConfiguration and Projections for full data loading

--- api/src/main/java/com/mmiranda/questionario/controller/ConviteController.java
+++ api/src/main/java/com/mmiranda/questionario/controller/ConviteController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/convites")
+@RequestMapping("/api/convites")
 public class ConviteController {
 
     private final ConviteRepository conviteRepository;
