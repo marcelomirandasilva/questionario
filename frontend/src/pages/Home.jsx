2f612c198b0ecd6f85b8c5b04f4b9648f8f2f4dc
marcelomirandasilva <marcelo.miranda.pp@gmail.com>
2026-01-08 16:16:42 -0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
       <div className="container">
         <header className="hero">
           <h1>Bem-vindo</h1>
-          <p>Portal em construção.</p>
+          <p>Portal em construção!!!</p>
         </header>
       </div>
     );
